eef2e336dad9eff1c16bd39548d02119f74b6023
Sean Yoro <sean.yoro@mindzie.com>
2022-06-26 19:27:13 +0800

    logging env vars

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Welcome to Pasakay!</div>;
+  return <div>{console.log(process.env)} Welcome to Pasakay!</div>;
 };
 
 export default Home;
